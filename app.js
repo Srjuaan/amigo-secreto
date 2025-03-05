d41fe3c8333dc6e44729008ce2f0ecb93564a457
Juanes Duarte <juaanes22@gmail.com>
2025-03-05 13:26:37 -0500

    Actualiza app

--- app.js
+++ app.js
@@ -1,1 +1,43 @@
-// empiezo el proyecto
+let totalAmigos = [];
+
+function agregarAmigo() {
+    let friend = document.getElementById('amigo').value;
+    totalAmigos.push(friend);
+    console.log(totalAmigos);
+    if (friend == '') {
+        invalido()
+    }
+    limpiarCaja();
+    asignarTextoElemento1('#listaAmigos',totalAmigos);
+}
+
+function limpiarCaja() {
+    document.querySelector('#amigo').value = '';
+}
+
+function asignarTextoElemento1(elemento, texto) {
+    let elementoHTML = document.querySelector(elemento);
+    elementoHTML.innerHTML = texto.join('<br>');
+    return;
+}
+
+function asignarTextoElemento(elemento, texto) {
+    let elementoHTML = document.querySelector(elemento);
+    elementoHTML.innerHTML = texto;
+    return;
+}
+
+function sortearAmigo() {
+    if (totalAmigos != '') {
+        let generaDatoRandom = Math.floor(Math.random() * totalAmigos.length);
+        let datoRandom = totalAmigos[generaDatoRandom];
+        console.log(datoRandom)
+        asignarTextoElemento('#resultado', `El dato sorteado es: ${datoRandom}`);
+    } else {
+        invalido()
+    }
+}
+
+function invalido() {
+    alert("Por favor inserte un nombre");
+}
